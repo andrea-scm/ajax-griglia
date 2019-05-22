e510f87a559bc8676469cd6e7fe143b63387a44b
andrea-scm <a.scimitto@gmail.com>
2019-05-22 11:49:55 +0200

    now square can be clicked only once

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ for (var i = 0; i < 36; i++) {
   $('.square_container').append('<div class="squares"> <span id = "num"></span> </div>');
 }
 
-
 $(document).ready(function () {
   $('.squares').click(function () {//in modo che ad ogni click del quadratino viene cambiato il numero
+    var current_square = $(this);
     $.ajax({
       'url': 'https://www.boolean.careers/api/random/int',
       'method': 'GET',
       'success': function (data) {
-        riempiQuadratino(data.response);
+          riempiQuadratino(data.response,current_square);
       },
       'error': function () {
         alert('errore');
@@ -18,17 +18,16 @@ $(document).ready(function () {
   });
 });
 
+
+
 //funzione che va a colorare il quadratino in base al numero e ne mostra il numero
-function riempiQuadratino(num) {
-  $('.squares').click(function () {
-    $(this).children('#num').text(num);
-    var squaresNum = $(this).children('#num').text();
-    if (squaresNum > 0 && squaresNum <= 5){
-      $(this).addClass('yellow');
-      $(this).removeClass('green');
+function riempiQuadratino(num,square) {
+    $(square).children('#num').text(num);
+    var squaresNum = $(square).children('#num').text();
+    if (squaresNum <= 5){
+      $(square).addClass('yellow');
     }else{
-      $(this).addClass('green');
-      $(this).removeClass('yellow');
-    }
-  });
+      $(square).addClass('green');
+    };
+    square.off();//in questo modo mi rende cliccabile il quadratino solo una volta
 }
